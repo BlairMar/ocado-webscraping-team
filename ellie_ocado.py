938f44f7edf4f1b152ad43bcd560d523630c0d04
EllieBrakoniecki <eleanor@brakoniecki.com>
2021-11-01 00:53:08 +0000

    Converting the functions to methods of the class and adding associated attribute

--- ellie_ocado.py
+++ ellie_ocado.py
@@ -1,35 +1,82 @@
 #%%
+### Imports:
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-from pprint import pprint
-import os
 import requests
-from uuid import uuid4
+import time
 
-#%% 
-### once we have a class make url_dictionary and driver attributes instead of params:
-def get_url_list_by_category(category_name, category_url, url_dictionary, driver): 
-    url_list = []    
-    driver.get(category_url)   
-    number_of_products_in_category = driver.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/div[1]/div[2]/div/span').text.split(' ')[0]
-    driver.get(category_url + "?display=" + number_of_products_in_category)  
-    ## Scroll to get 30 product url's at a time:
-    urls_tmp_web_object = []
-    n = int(int(number_of_products_in_category)/30)
-    for i in range(n):
-        driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight*{(i+1)/n});")
-        urls_tmp_web_object.extend(driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a'))
-        time.sleep(0.5)
-    urls_web_object = list(set(urls_tmp_web_object))
-    urls_web_object = driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a')
-    urls = [url.get_attribute('href') for url in urls_web_object]
-    # for url in urls:
-    #     url_list.append(url) # use a list as we need to scroll to find all the products
-    url_dictionary[category_name] = urls
+from pprint import pprint
+###
+#%%
+### Class Template:
+class OcadoScraper:
+    def __init__(self):
+        """
+        Initialiser: to decide what functions are called here and what attributes and arguments are defined
+        """
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.driver.maximize_window()
+        self.driver.get('https://www.ocado.com/')
+        try:
+            self._accept_cookies()
+        except:
+            pass
+        self.category_links = {}
+        self.product_links = {}
+        self.product_data = {}
 
+    def _accept_cookies(self):
+        """
+        Clicks Cookies button
+        Locate and Click Cookies Button
+        """
+        _accept_cookies = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+        _accept_cookies.click()
 
-def get_product_xpaths(key):
+    def _get_category_links(self):
+        if not self.category_links: # remove this probably
+            self.driver.get("https://www.ocado.com/browse")
+            categories_web_object = self.driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[1]/div/div/div[1]/div/div[1]/div/ul/li/a')
+            self.category_links = {category.text : category.get_attribute('href') for category in categories_web_object}
+
+    def _get_product_links(self, category_name): 
+        category_url = self.category_links[category_name]  
+        self.driver.get(category_url)  
+        number_of_products_in_category = self.driver.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/div[2]/div[2]/div/span').text.split(' ')[0]
+        self.driver.get(category_url + "?display=" + number_of_products_in_category)  
+        urls_tmp_web_object = []
+        n = int(int(number_of_products_in_category)/30) ## Scroll to get 30 product url's at a time:
+        for i in range(n):
+            self.driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight*{(i+1)/n});")
+            urls_tmp_web_object.extend(self.driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a'))
+            time.sleep(0.5)
+        urls_web_object = list(set(urls_tmp_web_object))
+        urls_web_object = self.driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a')
+        links = [url.get_attribute('href') for url in urls_web_object]
+        self.product_links[category_name] = links
+
+    def _get_product_data(self, category_name):
+        product_details = {} 
+        for i, url in enumerate(self.product_links[category_name]): ## remove enumerate - just for testing
+            self.driver.get(url)
+            name = self.driver.find_element(By.XPATH, self._get_product_xpaths('Name')).text
+            description = self.driver.find_element(By.XPATH, self._get_product_xpaths('Description')).text
+            price = self.driver.find_element(By.XPATH, self._get_product_xpaths('Price')).text
+            # price_per = self.driver.find_element(By.XPATH, self._get_product_xpaths('Price per')).text # doesn't exist for all items
+            rating = self.driver.find_element(By.XPATH, self._get_product_xpaths('Rating')).get_attribute('title').split(' ')[1]        
+            product_details[name] = { 'Description' : description,
+                                        'Price' : price,
+                                        #    'Price per' : price_per,
+                                        'Rating' : rating 
+                                        }  
+            if i == 1:  ### remove - just for testing
+                break
+        self.product_data[category_name] = product_details
+
+    @staticmethod
+    def _get_product_xpaths(key):
         product_xpaths = { 'Name' : '//*[@id="overview"]/section[1]/header/h2',
                            'Description' : '//*[@id="productInformation"]/div[2]/div[1]/div[2]/div/div[1]/div',
                            'Price' : '//*[@id="overview"]/section[2]/div[1]/div/h2',
@@ -38,65 +85,89 @@ def get_product_xpaths(key):
                          } 
         return product_xpaths[key]
 
+    def scrape_products(self, categories="ALL"):
+        self._get_category_links()
+        if categories == "ALL":
+            categories = self.category_links.keys()
+        for i, category in enumerate(categories):
+            self._get_product_links(category)
+            self._get_product_data(category)
+            if i == 0:
+                break
 
-def get_product_data_by_category(category_name, all_products_dict):
-    category_links = url_data[category_name]
-    product_details_dict = {} 
-    for i, url in enumerate(category_links): ## remove enumerate - just for testing
-        driver.get(url)
-        name = driver.find_element(By.XPATH, get_product_xpaths('Name')).text
-        description = driver.find_element(By.XPATH, get_product_xpaths('Description')).text
-        price = driver.find_element(By.XPATH, get_product_xpaths('Price')).text
-        # price_per = driver.find_element(By.XPATH, get_product_xpaths('Price per')).text # doesn't exist for all items
-        rating = driver.find_element(By.XPATH, get_product_xpaths('Rating')).get_attribute('title').split(' ')[1]        
-        product_details_dict[name] = { 'Description' : description,
-                                       'Price' : price,
-                                    #    'Price per' : price_per,
-                                       'Rating' : rating 
-                                    }  
-        if i == 1:  ### remove - just for testing
-            break
-    all_products_dict[category_name] = product_details_dict
+ 
+    def func2(self):
+        """
+        Gets Browse shop category URLs
+        """
+    
+    def func3(self):
+        """
+        Gets inner category urls from each Browse shop category
+        """
+    
+    def func4(self):
+        """
+        Opens URL in new tab
+        """
+
+    def func5(self):
+        """
+        Closes current tab (This function might be redundant)
+        """
+
+    def func6_0(self):
+        """
+        Gets number of items that can be displayed on page
+        """
+
+    def func6(self):
+        """
+        Displays all items on page
+        """
+
+    def func7(self):
+        """
+        Collects all the item URLs on a page
+        """
+    
+    def func8(self):
+        """
+        Gets images from item page
+        """
+    
+    def func9_0(self):
+        """
+        Gets price, description, SKU, reviews from item page
+        """
+    def func9(self):
+        """
+        Scrapes an item's page
+        """
+
+    def func10(self):
+        """
+        Creates folder in which to store data dictionaries if the folder doesn't already exist
+        """
+    
+    def func11(self):
+        """
+        Saves data into dictionary
+        """
+    
+    def func12(self):
+        """
+        Saves dictionary in a file
+        """
+    
+
+if __name__ == '__main__':
+    pass
 #%%
+ocado = OcadoScraper() 
+ocado.scrape_products()
+print(len(ocado.product_links["Christmas"]))
+print(ocado.category_links)
 
-## put into function
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-driver = webdriver.Chrome(options=options)
-driver.maximize_window()
-URL = "https://www.ocado.com/browse"
-driver.get(URL)
-
-## put into function
-cookie_button = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
-cookie_button.click()
-
-## create new function to get all the category url's 
-# fruits_url = 'https://www.ocado.com/browse/fresh-chilled-food-20002/fruit-44490'
-bakery_url = 'https://www.ocado.com/browse/bakery-25189'
-# fresh_and_chilled_url = 'https://www.ocado.com/browse/fresh-chilled-food-20002'
-url_data = {} ## make this an attribute
-## create function to get all the top level category urls:
-# get_url_list_by_category("Fruits", fruits_url, url_data, driver)
-get_url_list_by_category("Bakery", bakery_url, url_data, driver)
-# get_url_list_by_category("Fresh & Chilled Food", fresh_and_chilled_url, url_data, driver)
-# print(len(url_data['Fruits'])) 
-print(len(url_data['Bakery'])) 
-# print(len(url_data['Fresh & Chilled Food'])) 
-all_products_dict = {} ##make this attribute
-## iterate over all the categories once we have them
-# get_product_data_by_category('Fruits',all_products_dict) ## make all_products_dict an attribute
-get_product_data_by_category('Bakery',all_products_dict) ## make all_products_dict an attribute
-#%%
-# len(all_products_dict['Fruits'])
-len(all_products_dict['Bakery'])
-#%%
-for key, value in all_products_dict['Bakery'].items():
-    print(key)
-    print(value)
-
-# %%
-pprint(all_products_dict['Bakery'])
-# %%
 
 # %%
