850a3b65ba801145bd01ca45f5854eb026a19f64
EllieBrakoniecki <eleanor@brakoniecki.com>
2021-11-10 22:58:24 +0000

    Adding the rest of the attributes to scrape, adding some functions to support this functionality and support if the attribute is not there

--- OcadoScraper.py
+++ OcadoScraper.py
@@ -14,17 +14,17 @@ class OcadoScraper:
         """
         Access Ocado front page using chromedriver
         """
+        self.ROOT = 'https://www.ocado.com/' 
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument("--start-maximized")
         self.driver = webdriver.Chrome(options=self.chrome_options)
         self.driver.maximize_window()
-        self.driver.get('https://www.ocado.com/')
+        self.driver.get(self.ROOT)
         self._accept_cookies()
         self.category_urls = {}
         self.product_links = {}
         self.product_data = {}
         self._get_categories(scrape_categories) # populate category_links attribute
-
 
     def _accept_cookies(self):
         """
@@ -43,7 +43,6 @@ class OcadoScraper:
             with open("./data/category_urls") as f:
                 data = f.read()
                 self.category_urls = json.loads(data)
-
 
     def _scrape_category_urls(self):
         self.driver.get("https://www.ocado.com/browse")
@@ -65,14 +64,6 @@ class OcadoScraper:
         number_of_products = self.driver.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]//div/div[2]/div/span').text.split(' ')[0]
         return number_of_products
 
-    #######OLD:
-    # def _get_number_of_products_and_display_all(self, category_name):
-    #     category_page_link = self.category_links[category_name]  
-    #     self.driver.get(category_page_link)  
-    #     number_of_products = self.driver.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]//div/div[2]/div/span').text.split(' ')[0]
-    #     self.driver.get(category_page_link + "?display=" + number_of_products)
-    #     return number_of_products 
-
     def _scroll_to_get_all_product_links(self, number_of_products, number_of_items_in_each_scroll):
         urls_tmp_web_object = []
         n = int(int(number_of_products)/number_of_items_in_each_scroll)
@@ -87,33 +78,75 @@ class OcadoScraper:
 
     def _get_product_data(self, category_name):
         product_details = {} 
-        for i, url in enumerate(self.product_links[category_name]): ## remove enumerate - just for testing
+        for i, url in enumerate(self.product_links[category_name]): ## remove enumerate 
             self.driver.get(url)
-            name = self.driver.find_element(By.XPATH, self._get_product_xpaths('Name')).text
-            description = self.driver.find_element(By.XPATH, self._get_product_xpaths('Description')).text
-            price = self.driver.find_element(By.XPATH, self._get_product_xpaths('Price')).text
-            # price_per = self.driver.find_element(By.XPATH, self._get_product_xpaths('Price per')).text # doesn't exist for all items
-            rating = self.driver.find_element(By.XPATH, self._get_product_xpaths('Rating')).get_attribute('title').split(' ')[1]        
-            sku = OcadoScraper._get_sku_from_url(url)
-            product_details[sku] = {    'Name' : name,
-                                        'Description' : description,
-                                        'Price' : price,
-                                        #    'Price per' : price_per,
-                                        'Rating' : rating 
-                                        }  
-            if i == 3:  ### get the first i products - just for testing
-                break
+            product_attributes = {}
+            for key, value in self._get_attribute_xpaths().items():
+                attribute_web_element = self._get_attribute_by_xpath_or_none(key, value)
+                if attribute_web_element:
+                    if key in ['Name', 'Product Information', 'Price', 'Price per', 'Offers', 'Ingredients', 'Nutrition']:
+                        product_attributes[key] = attribute_web_element.text
+                    if key in ['Usage', 'Brand details']:
+                        product_attributes[key] = self._scrape_hidden_attributes(value)
+                    if key == 'Rating':
+                        product_attributes[key] = attribute_web_element.get_attribute('title').split(' ')[1]
+                    if key == 'Out of Stock':
+                       product_attributes[key] = True  
+                else:
+                    product_attributes[key] = False if key == 'Out of Stock' else None                                                              
+            product_details[self._get_sku_from_url(url)] = product_attributes
+            # if i == 10:  ### get the first i products - just for testing
+            #     break
         self.product_data[category_name] = product_details
 
+    def _scrape_hidden_attributes(self, xpath):
+        hidden_html_elements = self.driver.find_elements(By.XPATH, xpath)
+        text_in_hidden_elements = [element.get_attribute('textContent') for element in hidden_html_elements]
+        return (','.join(str(text) for text in text_in_hidden_elements))        
+
+    def _get_attribute_by_xpath_or_none(self, attribute_name, xpath):
+        """
+        Get a web element of the product attribute xpath.
+
+        params : 
+                attribute_name: name of the product attribute
+                xpath: xpath of the product attribute, eg xpath of the product name, price, description etc
+        return: 
+                The web element or None if nothing found.
+        """
+        try:
+            if attribute_name in ['Usage', 'Brand details']:
+                return self.driver.find_elements(By.XPATH, xpath)
+            return self.driver.find_element(By.XPATH, xpath)
+        except:
+            return None
+
     @staticmethod
-    def _get_product_xpaths(key):
+    def _get_attribute_xpaths():
+        """
+        Gets a dictionary containing the xpath for a product attribute.
+
+        return: 
+                A dictionary of xpaths for each product attribute
+
+        """
         product_xpaths = { 'Name' : '//*[@id="overview"]/section[1]/header/h2',
-                           'Description' : '//*[@id="productInformation"]/div[2]/div[1]/div[2]/div/div[1]/div',
+                           'Product Information' : '//*[@id="productInformation"]/div[2]/div[1]/div[2]/div',
                            'Price' : '//*[@id="overview"]/section[2]/div[1]/div/h2',
-                        #  'Price per' : '//*[@id="overview"]/section[2]/div[1]/div/span', # doesn't exist for all items
-                           'Rating' : '//*[@id="overview"]/section[1]/header/div/a[1]/div/span[1]/span'    
+                           'Price per' : '//*[@id="overview"]/section[2]/div[1]/div/span[2]',
+                           'Offers' : '//*[@id="overview"]/section[2]/div[2]/div/a/p',
+                           'Rating' : '//*[@id="overview"]/section[1]/header/div/a[1]/div/span[1]/span',
+                           'Ingredients' : '//*[@id="productInformation"]/div[3]/div/div[2]/div[2]/div/div[1]/div',
+                           'Usage' : '/html/body/div[1]/div[1]/div[3]/article/section[5]/div[2]/div[2]/div[2]//div/div',
+                           'Nutrition' : '//*[@id="productInformation"]/div[3]/div/div/div[2]/div/div/div/div/table/tbody',
+                           'Brand details' : '//html/body/div[1]/div[1]/div[3]/article/section[5]/div[2]/div[3]/div[2]//div/div',
+                           'Out of Stock' : '//*[@id="overview"]/section[2]/div[2]/h1'   
                          } 
-        return product_xpaths[key]
+        return product_xpaths
+    
+    @staticmethod
+    def _get_sku_from_url(url):
+        return url.split("-")[-1]
 
     def scrape_products(self, categories="ALL"):
         if categories == "ALL":
@@ -132,10 +165,6 @@ class OcadoScraper:
     def save_category_urls(self, mode='w'):
         with open('./data/category_urls', mode=mode) as f:
             json.dump(self.category_urls, f)
-
-    @staticmethod
-    def _get_sku_from_url(url):
-        return url.split("-")[-1]
         
     def zoom_page(self, zoom_percentage=100):
         self.driver.execute_script(f"document.body.style.zoom='{zoom_percentage}%'")
@@ -214,12 +243,14 @@ if __name__ == '__main__':
     # ocado.scrape_products()
 
 #%%
-
 ocado = OcadoScraper()
-categories_to_scrape = ["Health, Beauty & Personal Care"]
+categories_to_scrape = ["Bakery"]
 ocado.scrape_products(categories_to_scrape)
+print(len(ocado.product_links["Bakery"]))
 #%%
 ocado._get_product_links("Health, Beauty & Personal Care")  
 #%%
 print(len(ocado.product_links["Health, Beauty & Personal Care"]))
-
+categories_to_scrape = ["Baby, Parent & Kids"]
+ocado.scrape_products(categories_to_scrape)
+print(len(ocado.product_links["Baby, Parent & Kids"]))
