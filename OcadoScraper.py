8467e85b8d55ff933640ed2434b80b5deacab828
Ovidiu <popa.ovidiu98@gmail.com>
2021-11-11 18:40:59 +0000

    Adds multithreading functionality(WIP)

--- OcadoScraper.py
+++ OcadoScraper.py
@@ -6,6 +6,7 @@ import requests
 import time
 import json
 from pprint import pprint
+import threading
 ###
 #%%
 ### Class Template:
@@ -87,13 +88,13 @@ class OcadoScraper:
                     if key in ['Name', 'Product Information', 'Price', 'Price per', 'Offers', 'Ingredients', 'Nutrition']:
                         product_attributes[key] = attribute_web_element.text
                     if key in ['Usage', 'Brand details']:
-                        product_attributes[key] = self._scrape_hidden_attributes(attribute_web_element)
+                        product_attributes[key] = OcadoScraper._scrape_hidden_attributes(attribute_web_element)
                     if key == 'Rating':
                         product_attributes[key] = attribute_web_element.get_attribute('title').split(' ')[1]
                     if key == 'Out of Stock':
                        product_attributes[key] = True 
                     if key == 'Image links':
-                       product_attributes[key] = self._scrape_image_links(attribute_web_element)  
+                       product_attributes[key] = OcadoScraper._scrape_image_links(attribute_web_element)  
                 else:
                     product_attributes[key] = False if key == 'Out of Stock' else None                                                              
             product_details[OcadoScraper._get_sku_from_url(url)] = product_attributes
@@ -101,7 +102,44 @@ class OcadoScraper:
                 break
         self.product_data[category_name] = product_details
 
-    def _scrape_image_links(self, web_elements):
+    def _get_product_data_for_multithreading(self, category_name, url_list):
+        product_details = {}
+        for i in range(len(url_list)): ## remove enumerate 
+            self.driver.get(url_list[i])
+            product_attributes = {}
+            for key, value in OcadoScraper._get_attribute_xpaths().items():
+                attribute_web_element = self._get_attribute_by_xpath_or_none(key, value)
+                if attribute_web_element:
+                    if key in ['Name', 'Product Information', 'Price', 'Price per', 'Offers', 'Ingredients', 'Nutrition']:
+                        product_attributes[key] = attribute_web_element.text
+                    if key in ['Usage', 'Brand details']:
+                        product_attributes[key] = OcadoScraper._scrape_hidden_attributes(attribute_web_element)
+                    if key == 'Rating':
+                        product_attributes[key] = attribute_web_element.get_attribute('title').split(' ')[1]
+                    if key == 'Out of Stock':
+                       product_attributes[key] = True 
+                    if key == 'Image links':
+                       product_attributes[key] = OcadoScraper._scrape_image_links(attribute_web_element)  
+                else:
+                    product_attributes[key] = False if key == 'Out of Stock' else None
+            product_details[OcadoScraper._get_sku_from_url(url_list[i])] = product_attributes
+        return product_details
+
+    def _get_product_links_from_file(self):
+        with open("./data/product_urls") as f:
+            data = f.read()
+            self.product_links = json.loads(data)
+    
+    @staticmethod
+    def split_list(lst, n):
+        divided_list = []
+        for i in range(n):
+            divided_list.append(lst[i::n])
+        return divided_list
+
+
+    @staticmethod
+    def _scrape_image_links(web_elements):
         image_set = set() #use a set as if we have more than one image the large image will be counted twice
         for image in web_elements:
             image_src = image.get_attribute('src')
@@ -114,8 +152,9 @@ class OcadoScraper:
         image_list = list(image_set)
         #### now download the images? ### 
         return image_list 
-        
-    def _scrape_hidden_attributes(self, web_elements):
+    
+    @staticmethod
+    def _scrape_hidden_attributes(web_elements):
         text_in_hidden_elements = [element.get_attribute('textContent') for element in web_elements]
         return (' '.join(str(text) for text in text_in_hidden_elements))        
 
@@ -180,7 +219,51 @@ class OcadoScraper:
     def zoom_page(self, zoom_percentage=100):
         self.driver.execute_script(f"document.body.style.zoom='{zoom_percentage}%'")
 
+#%%
+#Class for multithreading
+class myThread(threading.Thread):
+    def __init__(self, threadID, url_list, category_name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.category_name = category_name
+        self.url_list = url_list
+        self.ocado_scraper = OcadoScraper()
+        
+    def run(self):
+        self.data = self.ocado_scraper._get_product_data_for_multithreading(self.category_name, self.url_list)
+#%%
+#test multithreading
+#This takes between 90 and 120 minutes to run
+# scraper_0 = OcadoScraper()
+# scraper_0._get_product_links_from_file()
+# list_of_url_lists = OcadoScraper().split_list(scraper_0.product_links["Fresh & Chilled Food"], 4)
+# scraper_0.driver.close()
 
+# threads = []
+
+# for i in range(len(list_of_url_lists)):
+#     threads.append(myThread(i, list_of_url_lists[i], "Fresh & Chilled Food"))
+
+# for thread in threads:
+#     thread.start()
+# print("done")
+
+#%%
+#continue test multithreading
+
+# print(threads[0].data)
+
+# total = {}
+
+# for thread in threads:
+#     total.update(thread.data)
+# print(len(total.items()))
+
+# total_2 = {"Fresh & Chilled Food": total}
+# with open('./data/product_data', 'w') as f:
+#     json.dump(total_2, f, indent=4)
+
+#%%
 if __name__ == '__main__':
 
     # ocado = OcadoScraper()
@@ -189,17 +272,18 @@ if __name__ == '__main__':
     pass
     # ocado = OcadoScraper() 
     # ocado.scrape_products()
+#%%
 
 #%%
-ocado = OcadoScraper()
-categories_to_scrape = ["Bakery"]
-ocado.scrape_products(categories_to_scrape)
-print(len(ocado.product_links["Bakery"]))
+# ocado = OcadoScraper(scrape_categories=True)
+# categories_to_scrape = "Fresh & Chilled Food"
+# ocado._get_product_links(categories_to_scrape)
+# print(len(ocado.product_links["Fresh & Chilled Food"]))
+# ocado._save_data('product_urls', ocado.product_links, mode='r')
+
 #%%
-ocado._get_product_links("Health, Beauty & Personal Care")  
-#%%
-ocado = OcadoScraper(True)
-categories_to_scrape = ["Baby, Parent & Kids"]
-ocado.scrape_products(categories_to_scrape)
-print(len(ocado.product_links["Baby, Parent & Kids"]))
+# ocado = OcadoScraper(True)
+# categories_to_scrape = ["Baby, Parent & Kids"]
+# ocado.scrape_products(categories_to_scrape)
+# print(len(ocado.product_links["Baby, Parent & Kids"]))
         
