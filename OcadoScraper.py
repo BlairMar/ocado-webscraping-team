9ff561f6673de403cfb6a14b0e95b356a755bb38
EllieBrakoniecki <eleanor@brakoniecki.com>
2021-11-13 21:14:10 +0000

    Merging from main

--- OcadoScraper.py
+++ OcadoScraper.py
@@ -7,8 +7,17 @@ import time
 import json
 import os
 from pprint import pprint
-###
-#%%
+
+import inspect
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from product import Product
+from images import Product_Images
+
 ### Class Template:
 class OcadoScraper:
     def __init__(self, scrape_categories=False):
@@ -23,9 +32,9 @@ class OcadoScraper:
         self.driver.get(self.ROOT)
         self._accept_cookies()
         self.category_urls = {}
-        self.product_links = {}
+        self.product_urls = {}
         self.product_data = {}
-        self._get_categories(scrape_categories) # populate category_links attribute
+        self._get_categories(scrape_categories) # populate category_urls attribute
 
     def _accept_cookies(self):
         """
@@ -34,17 +43,22 @@ class OcadoScraper:
         try:
             _accept_cookies = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
             _accept_cookies.click()
+            print("Cookies button clicked")
         except:
             print("No Cookies buttons found on page")
 
+############################################################################################
+# The following 3 functions are used to populate category_urls in the initialiser
+
+    # only used in the initialiser to get all the category urls (from a file if the file exists and scrape_categories=False)
     def _get_categories(self, scrape_categories=True):
         if scrape_categories:
             self._scrape_category_urls()
         else:
-            with open("./data/category_urls") as f:
-                data = f.read()
-                self.category_urls = json.loads(data)
+            self.category_urls = OcadoScraper.read_data("./data/category_urls")
 
+    # UTILITY function for above function
+    # only used by the function _get_categories() to scrape the category urls (and then save to a file)
     def _scrape_category_urls(self):
         self.driver.get(self.ROOT + "browse")
         self._accept_cookies()
@@ -53,158 +67,107 @@ class OcadoScraper:
         for category_name, category_url in self.category_urls.items():
             number_of_products = self._get_number_of_products(category_url)
             self.category_urls[category_name] += '?display=' + number_of_products
-        self._save_data("category_urls", self.category_urls, 'w')
+        OcadoScraper._save_data("category_urls", self.category_urls, 'w')
 
-    def _get_product_links(self, category_name):
-        number_of_products_in_category = self.category_urls[category_name].split('=')[-1]
-        self.driver.get(self.category_urls[category_name])
-        self.product_links[category_name] = self._scroll_to_get_all_product_links(number_of_products_in_category, 30)
-
+    # UTILITY function for above function
+    # only used in the function _scrape_category_urls() - gets the number of products in a category
     def _get_number_of_products(self, category_url):
         self.driver.get(category_url)
         number_of_products = self.driver.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]//div/div[2]/div/span').text.split(' ')[0]
         return number_of_products
+    
+###########################################################################################################################
+    #The following 2 functions are used to populate the product urls dictionary for the specified category
+    
+    # This function is called by the PUBLIC function scrape_products() and populates the product_urls dictionary for the specified category
+    def _scrape_product_urls(self, category_name):
+        number_of_products_in_category = self.category_urls[category_name].split('=')[-1]
+        self.driver.get(self.category_urls[category_name])
+        # self.product_urls[category_name] = self._scroll_to_get_all_product_urls(number_of_products_in_category, 30)
+        self.product_urls[category_name] = self._scroll_to_get_all_product_urls(30, 30)
 
-    def _scroll_to_get_all_product_links(self, number_of_products, number_of_items_in_each_scroll):
-        urls_tmp_web_object = []
-        n = int(int(number_of_products)/number_of_items_in_each_scroll)
-        for i in range(n):
-            self.driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight*{(i+1)/n});")
-            urls_tmp_web_object.extend(self.driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a'))
+    # UTILITY function for the above function to scroll the page and get all the product urls on the page
+    def _scroll_to_get_all_product_urls(self, number_of_products, number_of_items_in_each_scroll):
+        urls_temp_web_object = []
+        number_of_times_to_scroll = int(int(number_of_products)/number_of_items_in_each_scroll)
+        for i in range(number_of_times_to_scroll):
+            self.driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight*{(i+1)/number_of_times_to_scroll});")
+            urls_temp_web_object.extend(self.driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a'))
             time.sleep(0.5)
-        urls_web_object = list(set(urls_tmp_web_object))
+        urls_web_object = list(set(urls_temp_web_object))
         urls_web_object = self.driver.find_elements(By.XPATH, '//*[@id="main-content"]/div[2]/div[2]/ul/li/div[2]/div[1]/a')
-        links = [url.get_attribute('href') for url in urls_web_object]
-        return links
-
-    def _get_product_data(self, category_name, download_images):
+        urls = [url.get_attribute('href') for url in urls_web_object]
+        return urls
+    
+##############################################################################################################################
+# This function is called by the PUBLIC function scrape_products() and scrapes the information and images for 
+# all products in the category and puts them in the product_data dictionary
+ 
+    def _scrape_product_data(self, category_name, download_images):
         product_details = {} 
-        for i, url in enumerate(self.product_links[category_name]): ## remove enumerate 
-            self.driver.get(url)
-            product_sku = OcadoScraper._get_sku_from_url(url)
-            product_attributes = {}
-            for key, value in OcadoScraper._get_attribute_xpaths().items():
-                attribute_web_element = self._get_attribute_by_xpath_or_none(key, value)
-                if attribute_web_element:
-                    if key in ['Name', 'Product Information', 'Price', 'Price per', 'Offers', 'Ingredients', 'Nutrition']:
-                        product_attributes[key] = attribute_web_element.text
-                    if key in ['Usage', 'Brand details']:
-                        product_attributes[key] = OcadoScraper._scrape_hidden_attributes(attribute_web_element)
-                    if key == 'Rating':
-                        product_attributes[key] = attribute_web_element.get_attribute('title').split(' ')[1]
-                    if key == 'Out of Stock':
-                       product_attributes[key] = True 
-                    if key == 'Image links':
-                       product_attributes[key] = self._scrape_image_links(attribute_web_element, category_name, product_sku, download_images)  
-                else:
-                    product_attributes[key] = False if key == 'Out of Stock' else None                                                              
-            product_details[product_sku] = product_attributes
-            if i == 20:  ### get the first i+1 products - just for testing
+        for i, url in enumerate(self.product_urls[category_name]): ## remove enumerate 
+            product = Product(url)
+            sku = product.get_sku()
+            product_details[sku] = product.scrape_product_data(self.driver) 
+            if download_images:
+                product.download_images()
+            if i == 10:  ### get the first i+1 products - just for testing
                 break
         self.product_data[category_name] = product_details
+              
+##################################################################################################################  
+    # function to read data from a json file
+    @staticmethod
+    def read_data(path):
+        with open(path) as f:
+            data = f.read()
+            return json.loads(data) 
+  
+    # function to dump the data to a json file. Used to save the category_urls, product_links and product_data dictionarys to file
+    @staticmethod
+    def _save_data(filename, data, mode='a'):
+        path = f'./data/{filename}'
+        with open(path, mode=mode) as f:
+            json.dump(data, f) 
 
-    def _scrape_image_links(self, web_elements, category_name, product_sku, download_images):
-        image_set = set() # use a set as if we have more than one image the large image will be counted twice
-        for image in web_elements:
-            image_src = image.get_attribute('src')
-            if "640x640" in image_src:
-                image_set.add(image_src.replace("640x640", "1280x1280"))
-            else: 
-                image_set.add(image_src.replace("75x75", "1280x1280"))
-        image_list = list(image_set)
-        if download_images:
-            OcadoScraper._download_all_images(image_list, category_name, product_sku)
-        return image_list 
+###################################################################################################################
+    # PUBLIC FUNCTIONS
     
-    @staticmethod
-    def _download_all_images(image_list, category_name, product_sku):
-        path = f'./data/images/{category_name}/{product_sku}'
-        OcadoScraper._create_image_folder_if_not_exist(path)
-        for image_url in image_list:
-            image_number = image_url.split("_")[1] # image number 0 is main picture
-            OcadoScraper._download_img(image_url, path + f'/{image_number}.jpg')
-        
-    @staticmethod
-    def _create_image_folder_if_not_exist(path):
-        if not os.path.exists(path):
-            os.makedirs(path)
-                
-    @staticmethod
-    def _download_img(url, path):
-        img_data = requests.get(url).content
-        with open(path, 'wb') as handler:
-            handler.write(img_data)
+    # returns a list of categories available to scrape on the ocado website
+    # if from_file=True returns a list of saved categories from a previous scrape else gets the categories from the website
+    def categories_available_to_scrape(self, from_file=True):
+        if from_file == True:
+            temp_dict = OcadoScraper.read_data("./data/category_urls")
+            return list(temp_dict.keys())
+        else:
+            self._get_categories(True)
+            return list(self.category_urls.keys())
+               
+    def get_categories_with_saved_product_data():
+        pass
+    # read the json dict 
     
-    @staticmethod
-    def _scrape_hidden_attributes(web_elements):
-        text_in_hidden_elements = [element.get_attribute('textContent') for element in web_elements]
-        return (' '.join(str(text) for text in text_in_hidden_elements))        
-
-    def _get_attribute_by_xpath_or_none(self, attribute_name, xpath):
-        """
-        Get a web element of the product attribute xpath.
-
-        params : 
-                attribute_name: name of the product attribute
-                xpath: xpath of the product attribute, eg xpath of the product name, price, description etc
-        return: 
-                The web element or None if nothing found.
-        """
-        try:
-            if attribute_name in ['Usage', 'Brand details', 'Image links']:
-                return self.driver.find_elements(By.XPATH, xpath)
-            return self.driver.find_element(By.XPATH, xpath)
-        except:
-            return None
-
-    @staticmethod
-    def _get_attribute_xpaths():
-        """
-        Gets a dictionary containing the xpath for a product attribute.
-
-        return: 
-                A dictionary of xpaths for each product attribute
-
-        """
-        product_xpaths = { 'Name' : '//*[@id="overview"]/section[1]/header/h2',
-                           'Product Information' : '//*[@id="productInformation"]/div[2]/div[1]/div[2]/div',
-                           'Price' : '//*[@id="overview"]/section[2]/div[1]/div/h2',
-                           'Price per' : '//*[@id="overview"]/section[2]/div[1]/div/span[2]',
-                           'Offers' : '//*[@id="overview"]/section[2]/div[2]/div/a/p',
-                           'Rating' : '//*[@id="overview"]/section[1]/header/div/a[1]/div/span[1]/span',
-                           'Ingredients' : '//*[@id="productInformation"]/div[3]/div/div[2]/div[2]/div/div[1]/div',
-                           'Usage' : '/html/body/div[1]/div[1]/div[3]/article/section[5]/div[2]/div[2]/div[2]//div/div',
-                           'Nutrition' : '//*[@id="productInformation"]/div[3]/div/div/div[2]/div/div/div/div/table/tbody',
-                           'Brand details' : '//html/body/div[1]/div[1]/div[3]/article/section[5]/div[2]/div[3]/div[2]//div/div',
-                           'Out of Stock' : '//*[@id="overview"]/section[2]/div[2]/h1',
-                           'Image links' : '//*[@class="bop-gallery__miniatures"]//img | /html/body/div[1]/div[1]/div[3]/article/section[1]/div/div/div[1]/img'   
-                         } 
-        return product_xpaths    
+    def get_categories_without_saved_product_data():
+        pass
     
-    @staticmethod
-    def _get_sku_from_url(url):
-        return url.split("-")[-1]
-
+    # public function to scrape the products. Pass in a list of categories as a param. If there is saved product data this will be overwritten if we scrape again for the category
     def scrape_products(self, categories="ALL", download_images=False):
         if categories == "ALL":
             categories = self.category_urls.keys()        
         for category in categories:
-            self._get_product_links(category)
-            self._get_product_data(category, download_images)
-        self._save_data("product_links", self.product_links)
-        self._save_data("product_data", self.product_data)
-        print(f"Product links and product data from the {categories} categories saved successfully")
+            #### now read the data from the json dict into product_data attribute, clear the data from the json 
+            self._scrape_product_urls(category)
+            self._scrape_product_data(category, download_images)
+            OcadoScraper._save_data("product_data", self.product_data) #save data into the file after each scrape of a category
+        # save each time we scrape - put inside for loop
+        OcadoScraper._save_data("product_urls", self.product_urls)     
+        print(f"Product urls and product data from the {categories} categories saved successfully")
  
-    # def scrape_all_products(self):
-    #     for category in self.category_urls.keys():
-    #         scrape_sub_categorys(category)
-
-    def _save_data(self, filename, data, mode='a'):
-        with open(f'./data/{filename}', mode=mode) as f:
-            json.dump(data, f) 
-        
+####################################################################### 
+# This is not being used right now       
     def zoom_page(self, zoom_percentage=100):
         self.driver.execute_script(f"document.body.style.zoom='{zoom_percentage}%'")
+#######################################################################
 
 
 if __name__ == '__main__':
@@ -214,19 +177,15 @@ if __name__ == '__main__':
 
 #%%
 ocado = OcadoScraper()
-categories_to_scrape = ["Bakery"]
+# categories_to_scrape = ["Clothing & Accessories", "Bakery"]
+categories_to_scrape = ["Clothing & Accessories", 'Bakery']
 ocado.scrape_products(categories_to_scrape, True)
-print(len(ocado.product_links["Bakery"]))
-#%%
-ocado._get_product_links("Health, Beauty & Personal Care")  
-#%%
-ocado = OcadoScraper(True)
-categories_to_scrape = ["Baby, Parent & Kids"]
-ocado.scrape_products(categories_to_scrape)
-print(len(ocado.product_links["Baby, Parent & Kids"]))
-
+# ocado.scrape_products(categories_to_scrape)
+# print(len(ocado.product_urls["Clothing & Accessories"]))
 #%%
 ocado = OcadoScraper()
-ocado.scrape_all_products()
-    
+#%%
+ocado.categories_available_to_scrape()
+
 # %%
+ocado.categories_available_to_scrape(False)
