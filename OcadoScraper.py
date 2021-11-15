9ebef567679294a0936fa0831f0d3895b6620e43
Ovidiu <popa.ovidiu98@gmail.com>
2021-11-15 11:07:26 +0000

    Adds comment

--- OcadoScraper.py
+++ OcadoScraper.py
@@ -11,6 +11,7 @@ from pprint import pprint
 import inspect
 import sys
 
+#### For importing files in the repo
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
