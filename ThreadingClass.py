4a4bf05c4043f3c2997c78030a36c204bb6d94eb
Ovidiu <popa.ovidiu98@gmail.com>
2021-11-17 15:10:22 +0000

    Adds threading class for scraping product pages

--- ThreadingClass.py
+++ ThreadingClass.py
@@ -35,23 +35,39 @@ class CategoryPageThread(threading.Thread):
         self.product_urls = self.func(self.driver, self.number_of_scrolls, self.start_scrolling_at, self.stop_scrolling_at)
         self.active = False
 
+class ScrapingProductsThread(threading.Thread):
+    def __init__(self, threadID, url_list, func, download_images=False):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.url_list = url_list
+        self.func = func
+        self.download_images = download_images
+        self.product_details = {}
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.driver.get(self.url)
+        CategoryPageThread._accept_cookies(self.driver)
+        self.active = True
+    
+    @staticmethod
+    def _accept_cookies(driver):
+        """
+        Locate and Click Cookies Button
+        """
+        try:
+            _accept_cookies = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+            _accept_cookies.click()
+            print("Cookies button clicked")
+        except:
+            print("No Cookies buttons found on page")
+
+    def run(self):
+        l = len(self.url_list)
+        for i, url in enumerate(self.url_list):
+            if i%100 == 0:
+                print(f'Thread {self.threadID} is {int(i/l*100)}% done')
+            self.product_details[url.split("-")[-1]] = self.func(self.driver, url, self.download_images)
+        self.active = False
 
 
-
-
-
-
-
-
-# class myThread(threading.Thread):
-#     def __init__(self, driver, threadID, url_list, function):
-#         threading.Thread.__init__(self)
-#         self.threadID = threadID
-#         self.url_list = url_list
-#         self.function = function
-#         self.chrome_options = webdriver.ChromeOptions()
-#         self.chrome_options.add_argument("--start-maximized")
-#         self.driver = webdriver.Chrome(options=self.chrome_options)
-        
-#     def run(self):
-#         self.function(self.url_list)
