edc5e81ebf7e21e1be5c2d9738d460f15a59a5ee
Ellie Brakoniecki <91092044+EllieBrakoniecki@users.noreply.github.com>
2021-11-30 23:47:37 +0000

    Adding an sql module and folder to keep all the processing files in

---
+++ data_processing/sql.py
@@ -0,0 +1,11 @@
+import sys
+import inspect
+import os
+
+#### For importing files in the repo
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from sqlalchemy import create_engine
+import boto3
